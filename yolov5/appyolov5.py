f7640ae0eaa45ca7bbec911122b1916bed848876
ZeroX111 <97372800+ZeroX111@users.noreply.github.com>
2023-05-07 20:39:39 +0800

    Update appyolov5.py

--- yolov5/appyolov5.py
+++ yolov5/appyolov5.py
@@ -28,7 +28,7 @@ WEBRTC_CLIENT_SETTINGS = ClientSettings(     # webrtc_streamer配置
 
 # Custom Model
 # MODEL = torch.hub.load('ultralytics/yolov5', 'custom', path='/home/ubuntu/yolov5/best.pt')
-weights='./yolov5.pt'  # model path or triton URL
+weights='./yolov5s.pt'  # model path or triton URL
 device = select_device('')
 imgsz=(640, 640)
 
